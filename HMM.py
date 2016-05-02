4f640155b3bb8ceaac20182b60629e09d726ee70
Joshua Howard <joshthoward@gmail.com>
2016-05-02 12:58:38 -0400

    Update HMM.py

--- HMM.py
+++ HMM.py
@@ -1,56 +1,12 @@
 import numpy as np
 
-from sklearn import preprocessing
-
-# Map data set to numeric data
-O = preprocessing.LabelEncoder()
-H = preprocessing.LabelEncoder()
-O.fit(['b','g','y','r'])
-H.fit(['1:1','1:2','1:3','1:4','2:1','2:2','2:3','2:4',\
-       '3:1','3:2','3:3','3:4','4:1','4:2','4:3','4:4'])
-
-# Read data set and calculate statistics
-def readData():
-    
-    A = np.zeros((16,16))
-    B = np.zeros((16,4))
-    I = np.zeros((16))
-    
-    #Get observation counts
-    with open('../Robot/robot_train.data', 'r') as file:
-        first = 0
-        for line in file:
-            
-            if (line != '.\n'):
-                first += 1
-                
-                split = line.strip('\n').split(" ")
-                i  =  H.transform(split[0])
-                B[i,O.transform(split[1])] += 1
-                
-                if (first != 1):
-                    A[i,j] += 1
-                j = i
-        
-            else:
-                I[H.transform(split[0])] += 1
-                first = 0
-
-    # Normalize counts
-    I /= np.sum(I)
-    
-    for i in range(A.shape[1]):
-        sum = np.sum(A[:,i])
-        if (sum != 0):
-            A[:,i] /= sum
-
-    for i in range(B.shape[0]):
-        sum = np.sum(B[i,:])
-        if (sum != 0):
-            B[i,:] /= sum
-
-    return I, A, B
-
+'''
+Viterbi Decoding of Hidden Markov Model (HMM) with parameters 
+I - Initial probabilities
+A - Transition probabilities
+B - Emission probabilities
+y - Sequence of observations
+'''
 
 def Viterbi(y, I, A, B):
     
@@ -72,29 +28,3 @@ def Viterbi(y, I, A, B):
             x.append(S[x[-1],i])
 
         return np.array(x[::-1])
-
-def main():
-
-    I, A, B = readData()
-    acc = []
-    with open('../Robot/robot_test.data', 'r') as file:
-        sequence = []
-        labels   = []
-        for line in file:
-            if (line != '.\n'):
-                temp = line.strip('\n').split(" ")
-                sequence.append(temp[1])
-                labels.append(temp[0])
-            else:
-                sequence = O.transform(sequence)
-                labels = H.transform(labels)
-                predicted = Viterbi(sequence,I,A,B)
-                acc.append(int(np.average(labels == predicted) * 100) / 100.0)
-                sequence = []
-                labels = []
-
-    print np.average(np.array(acc))
-
-
-if __name__ == '__main__':
-    main()
